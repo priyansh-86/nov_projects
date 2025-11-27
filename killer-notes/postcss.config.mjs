434836d60ec7eddbb6c3501b72b3f9749ad6a146
priyansh-86 <priyanshrajbhar499@gmail.com>
2025-11-27 09:20:00 +0530

    updating movie site

--- killer-notes/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // <-- Yahan change kiya hai
-    autoprefixer: {},
-  },
-};
-
-export default config;
